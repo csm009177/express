ae5410412bd9068bd075ae4f057d833bee60431d
csm <csm009177@gmail.com>
2023-10-31 10:21:21 +0900

    launch serv success

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // import module 
 const express = require('express');
-const app = express;
+const app = express();
 const bodyParser = require('body-parser')
-
+// port 
 const port = 2317;
+// 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.get('/', (req,res)=> {
+  res.send('hello');
+})
+
+app.listen(port, ()=> {
+  console.log(`
+http://localhost:${port}  
+  `)
+});
