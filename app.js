159e867551596db13ad50ff21f01ebf1c4762e79
csm <csm009177@gmail.com>
2023-11-01 20:00:26 +0900

    make login form for main.html

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 // import module 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 // port 
 const port = 2317;
 
-
 // get
 app.get('/', (req,res)=> {
-  res.send('hello');
+  res.sendFile(__dirname +'/main.html');
 })
 //post
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: false }));
-app.post('/login', (req,res)=> {
-  res.send('hello world')
+app.post('/login', (req, res) => {
+  // make body
+  const userid = req.body.userid;
+  const userpw = req.body.userpw;
+  
+  const goodid = 'csm';
+  const goodpw = '0177';
+  
+  if ( goodid === userid&& goodpw === userpw) {
+    res.sendFile( __dirname + '/index.html');
+  } else {
+    res.send('Invalid login');
+  }
 });
-
 
 app.listen(port, ()=> {
   console.log(`
