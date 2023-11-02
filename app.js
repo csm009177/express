be2fc33848548f8ef4f37a2c6f6c1a37f748dd46
csm <csm009177@gmail.com>
2023-11-02 23:23:20 +0900

    sendFile good

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const app = express();
 // port
 const port = 2317;
 
+// 정적 파일을 제공하기 위한 미들웨어 설정
+app.use(express.static('public'));
+
+// sendfile
 app.get('/', (req,res)=> {
-  res.send('hello')
+  res.sendFile('./index.html')
 })
+
+
 
 app.listen(port, () =>{
   console.log(`
