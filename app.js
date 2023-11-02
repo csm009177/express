296c23f48979a942ea6a58ba1db9183a1ffb8794
csm <csm009177@gmail.com>
2023-11-02 23:17:56 +0900

    make new server

--- app.js
+++ app.js
@@ -1,35 +1,15 @@
-// import module 
+// import 
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.json());
-// port 
+// port
 const port = 2317;
 
-// get
 app.get('/', (req,res)=> {
-  res.sendFile(__dirname +'/index.html');
+  res.send('hello')
 })
-//post
-app.post('/login', (req, res) => {
-  // make body
-  const userid = req.body.userid;
-  const userpw = req.body.userpw;
-  
-  const goodid = 'csm';
-  const goodpw = '0177';
-  
-  if ( userid=== goodid && userpw === goodpw ) {
-    res.sendFile( __dirname + '/search.html');
-  } else {
-    res.send('Invalid login');
-  }
-});
 
-app.listen(port, ()=> {
+app.listen(port, () =>{
   console.log(`
-http://localhost:${port}  
+http://localhost:${port}
   `)
-});
+})
