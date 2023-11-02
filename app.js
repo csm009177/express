f96dd3a6739e546a970fe99594bcac65a3db9555
csm <csm009177@gmail.com>
2023-11-02 23:27:30 +0900

    make Id check

--- app.js
+++ app.js
@@ -7,9 +7,21 @@ const port = 2317;
 // 정적 파일을 제공하기 위한 미들웨어 설정
 app.use(express.static('public'));
 
-// sendfile
+
+
+// get sendFile
 app.get('/', (req,res)=> {
-  res.sendFile('./index.html')
+  res.sendFile('./index.html');
+})
+
+app.post('/login', (req,res)=> {
+  const userId= req.body.Id;
+  const userPw= req.body.Pw
+
+
+  if(userId===goodId && userPw === goodPw){
+    res.send('./search.html');
+  }
 })
 
 
