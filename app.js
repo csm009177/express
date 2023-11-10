d66edb45c3f22decc0a61eeeeafb809a544212aa
csm <csm009177@gmail.com>
2023-11-10 19:20:47 +0900

    success restore

--- app.js
+++ app.js
@@ -1,43 +1,37 @@
-import http from 'http';
-import express from 'express'
-import fs from 'fs';
+// import 
+const express = require('express');
 const app = express();
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(express.json());
-
-// port 
+// port
 const port = 2317;
 
 // 정적 파일을 제공하기 위한 미들웨어 설정
 app.use(express.static('public'));
 
 // 데이터를 파싱하기 위한 선언 
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
-//get sendFile
+// get sendFile
 app.get('/', (req,res)=> {
-  res.sendFile(__dirname + './public/login.html');
+  res.sendFile('./public/index.html');
 })
 
-//post
-app.post('/login', (req, res) => {
-  // make body
-  const userid = req.body.userid;
-  const userpw = req.body.userpw;
-  
-  const goodid = 'csm';
-  const goodpw = '0177';
-  
-  if ( userid=== goodid && userpw === goodpw ) {
-    res.sendFile( __dirname + './public/index.html');
-  } else {
-    res.send('Invalid login');
-  }
-});
+app.post('/login', (req,res)=> {
+  // import id and pw
+  const userId= req.body.Id;
+  const userPw= req.body.Pw
+  // goot id and pw
+  const goodId= 'csm';
+  const goodPw= '0177';
 
-app.listen(port, ()=> {
+  if(userId===goodId && userPw === goodPw){
+    res.sendFile(__dirname + '/public/search.html');
+  }
+})
+
+
+
+app.listen(port, () =>{
   console.log(`
 http://localhost:${port}
   `)
